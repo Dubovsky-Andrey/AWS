ccb1c01dd03bcd7c46d3d3cf57d2c35b8267dcf9
John Doe <dubovsky.andrey.work@gmail.com>
2023-09-07 14:52:31 -0400

    update

--- ubuntu-2204/conf-mysql-server.sh
+++ ubuntu-2204/conf-mysql-server.sh
@@ -1,37 +1,32 @@
 #!/bin/bash
 
-# Automatically set up MySQL
-# WARNING: This is only for demonstration purposes and not recommended for production environments
+# Установка MySQL, если она ещё не установлена
 
-# Update MySQL root password
-sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password1!';"
+# Автоматический запуск mysql_secure_installation
+sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '1Moodle!';"
 
-# Run mysql_secure_installation silently
-sudo mysql_secure_installation <<EOF
+# Ответы на вопросы mysql_secure_installation
+expect_secure_mysql=$(expect -c "
+set timeout 3
+spawn mysql_secure_installation
+expect \"Enter password for user root:\"
+send \"1Moodle!\r\"
+expect \"Press y|Y for Yes, any other key for No:\"
+send \"y\r\"
+expect \"Please enter 0 = LOW, 1 = MEDIUM and 2 = STRONG:\"
+send \"1\r\"
+expect \"Change the password for root ?\"
+send \"n\r\"
+expect \"Remove anonymous users?\"
+send \"y\r\"
+expect \"Disallow root login remotely?\"
+send \"n\r\"
+expect \"Remove test database and access to it?\"
+send \"y\r\"
+expect \"Reload privilege tables now?\"
+send \"n\r\"
+expect eof
+")
 
-# Enter root password1!
-password1!
-
-# Setup VALIDATE PASSWORD component
-y
-
-# Choose password strength level
-1
-
-# Do not change root password
-n
-
-# Remove anonymous users
-y
-
-# Allow remote root login (for demonstration purposes only, not safe for production)
-n
-
-# Remove test database
-y
-
-# Reload privilege tables
-n
-EOF
-
-echo "MySQL setup completed."
+# Выводить процесс выполнения скрипта
+echo "$expect_secure_mysql"
