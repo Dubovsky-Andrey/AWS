1acb7cd094bb2d4aad547a57ea763bb792c47e6c
John Doe <dubovsky.andrey.work@gmail.com>
2023-09-07 16:26:27 -0400

    update

--- ubuntu-2204/0011-clone-moodle.sh
+++ ubuntu-2204/0011-clone-moodle.sh
@@ -23,6 +23,24 @@ git branch --track $MOODLE origin/$MOODLE
 # Check out the specified Moodle version
 git checkout $MOODLE
 
-# Print a success message
+# Print a message
 echo "Moodle version $MOODLE has been successfully downloaded and setup."
+
+# Step 5: Copy local repository to /var/www/html/
+
+# Copy moodle directory to /var/www/html/
+cp -R /opt/moodle /var/www/html/
+
+# Create moodledata directory
+mkdir /var/moodledata
+
+# Change ownership to www-data
+chown -R www-data /var/moodledata
+
+# Set permissions
+chmod -R 777 /var/moodledata
+chmod -R 0755 /var/www/html/moodle
+
+# Print a success message
+echo "Moodle has been successfully copied to /var/www/html/ and permissions have been set."
 
